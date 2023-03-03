c780f8340a56896fee4b3d9140098863f7f1d581
Kutay Tanyıldızı <123557728+kutaytanyildizi@users.noreply.github.com>
2023-03-03 11:54:36 +0300

    Update main.cpp

--- C++ Time Spend in Function/main.cpp
+++ C++ Time Spend in Function/main.cpp
@@ -3,43 +3,45 @@
 #include <chrono>
 #include <sstream>
 #include <iomanip>
+#pragma warning(disable : 4996)
 
-void vecDisplay(const std::vector<int> &numbers);
+void vecDisplay(const std::vector<int>& numbers);
 void arrDisplay(const int numbers[]);
+void startingTime(std::string nameOfDataStructure);
+void endingTime(std::string nameOfDataStructure);
 
 int main()
 {
-    std::vector<int> vec = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
-    int arr[] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+    std::vector<int> vec = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
+    int arr[] = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
 
     vecDisplay(vec);
     arrDisplay(arr);
 }
 
-void vecDisplay(const std::vector<int> &numbers)
+void vecDisplay(const std::vector<int>& numbers)
 {
-    auto now = std::chrono::system_clock::now();
-    auto time = std::chrono::system_clock::to_time_t(now);
-    auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch()) % 1000;
-    std::tm* utc_time = std::gmtime(&time);
-    std::ostringstream time_ss;
-    time_ss << std::put_time(utc_time, "%Y-%m-%d %H:%M:%S:") << std::setfill('0') << std::setw(3) << ms.count();
-    std::cout << "Vector Display started at: " << time_ss.str() << std::endl;
-    for(const auto it: numbers)
+    startingTime("Vector");
+    for (const auto it : numbers)
     {
         std::cout << it << " ";
     }
     std::cout << std::endl;
-    now = std::chrono::system_clock::now();
-    time = std::chrono::system_clock::to_time_t(now);
-    ms = std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch()) % 1000;
-    utc_time = std::gmtime(&time);
-    time_ss.str("");
-    time_ss << std::put_time(utc_time, "%Y-%m-%d %H:%M:%S:") << std::setfill('0') << std::setw(3) << ms.count();
-    std::cout << "Vector Display ended at: " << time_ss.str() << std::endl;
+    endingTime("Vector");
 }
 
 void arrDisplay(const int numbers[])
+{
+    startingTime("Array");
+    for (int i = 0; i < 10; i++)
+    {
+        std::cout << numbers[i] << " ";
+    }
+    std::cout << std::endl;
+    endingTime("Array");
+}
+
+void startingTime(std::string nameOfDataStructure)
 {
     auto now = std::chrono::system_clock::now();
     auto time = std::chrono::system_clock::to_time_t(now);
@@ -47,17 +49,16 @@ void arrDisplay(const int numbers[])
     std::tm* utc_time = std::gmtime(&time);
     std::ostringstream time_ss;
     time_ss << std::put_time(utc_time, "%Y-%m-%d %H:%M:%S:") << std::setfill('0') << std::setw(3) << ms.count();
-    std::cout << "Array Display started at: " << time_ss.str() << std::endl;
-    for(int i = 0; i < 10; i++)
-    {
-        std::cout << numbers[i] << " ";
-    }
-    std::cout << std::endl;
-    now = std::chrono::system_clock::now();
-    time = std::chrono::system_clock::to_time_t(now);
-    ms = std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch()) % 1000;
-    utc_time = std::gmtime(&time);
-    time_ss.str("");
+    std::cout << nameOfDataStructure << " Display started at: " << time_ss.str() << std::endl;
+}
+
+void endingTime(std::string nameOfDataStructure)
+{
+    auto now = std::chrono::system_clock::now();
+    auto time = std::chrono::system_clock::to_time_t(now);
+    auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch()) % 1000;
+    std::tm* utc_time = std::gmtime(&time);
+    std::ostringstream time_ss;
     time_ss << std::put_time(utc_time, "%Y-%m-%d %H:%M:%S:") << std::setfill('0') << std::setw(3) << ms.count();
-    std::cout << "Array Display ended at: " << time_ss.str() << std::endl;
+    std::cout << nameOfDataStructure << " Display ended at : " << time_ss.str() << std::endl;
 }
