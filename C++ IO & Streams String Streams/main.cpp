3e817c1516dcd1ba04eca442b25af0846f55b28e
Kutay Tanyıldızı <123557728+kutaytanyildizi@users.noreply.github.com>
2023-03-07 16:05:58 +0300

    Add files via upload

---
+++ C++ IO & Streams String Streams/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <string>
+#include <limits> 
+
+int main()
+{
+    int num{};
+    double total{};
+    std::string name{};
+
+    std::string info{"Moe 100 1234.5"};
+    std::istringstream iss{info};
+
+    iss >> name >> num >> total;
+    std::cout << std::setw(10) << std::left << name << std::setw(10) << std::left << num << std::setw(10) << std::left << total << std::endl;
+
+    std::cout << "-----------------------------" << std::endl;
+    std::ostringstream oss{};
+    oss << std::setw(10) << std::left << name << std::setw(10) << std::left << num << std::setw(10) << std::left << total << std::endl;
+    std::cout << oss.str() << std::endl;
+
+    std::cout << "-----Doing Validation with StringStreams-----" << std::endl;
+    int value{};
+    std::string entry{};
+    bool done = false;
+    do
+    {
+        std::cout << "Please enter an integer for validation operation : ";
+        std::cin >> entry;
+        std::istringstream validator{entry};     
+        if(validator >> value)
+        {
+            done = true;
+        }
+        else
+        {
+            std::cout << "Sorry, that's not an integer." << std::endl;
+        }
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
+    } while (!done);
+	return 0;
+}
