6f44032cd260e36d48919d049681cadbbc19674c
Kutay Tanyıldızı <123557728+kutaytanyildizi@users.noreply.github.com>
2023-03-14 09:14:01 +0300

    Update main.cpp

--- C++ Singleton Design Pattern/main.cpp
+++ C++ Singleton Design Pattern/main.cpp
@@ -5,7 +5,7 @@
 /*
 1) Singleton is a creational design pattern that lets you ensure that a class has only one instance, while providing a global access point to this instance.
 2) One of the most common reasons for wanting a single instance of the class is to control access to some shared resource, such as a database or file.
-   There is a another side to want a single instance of the class you don't want the code that solves problem #1 to be scratted all over your program. 
+   There is a another side to want a single instance of the class you don't want the code that solves problem #1 to be scratted all over your program.
 3) Imagine that you created an object, but after a while decided to create a new one. Instead of receiving a fresh object, you’ll get the one you already created.
 */
 
@@ -18,7 +18,7 @@ private:
     friend std::ostream &operator<<(std::ostream &os, const Singleton &sngl)
     {
         os << sngl.getData();
-        return os; 
+        return os;
     }
 
 public:
@@ -29,7 +29,7 @@ public:
             singleton = new Singleton;
         }
         return singleton;
-        
+
     }
     Singleton(const Singleton&) = delete;
     Singleton(Singleton&&) = delete;
@@ -45,14 +45,16 @@ public:
     }
 };
 
+Singleton *Singleton::singleton = nullptr;
+
 int main()
 {
     Singleton* singleton1 = singleton1->getInstance();
     singleton1->setData(20);
-    std::cout << "Singleton 1 Data = " << singleton1 << std::endl;
+    std::cout << "Singleton 1 Data = " << *singleton1 << std::endl;
     Singleton* singleton2 = Singleton::getInstance();
-    std::cout << "Singleton 2 Data = " << singleton2 << std::endl;
+    std::cout << "Singleton 2 Data = " << *singleton2 << std::endl;
     Singleton::getInstance()->setData(50);
-    std::cout << "Singleton 1 Data = " << singleton1 << std::endl;
-    std::cout << "Singleton 2 Data = " << singleton2 << std::endl;
+    std::cout << "Singleton 1 Data = " << *singleton1 << std::endl;
+    std::cout << "Singleton 2 Data = " << *singleton2 << std::endl;
 }
